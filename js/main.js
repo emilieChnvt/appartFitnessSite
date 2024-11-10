f1a05c00a28d6a89a3a815c88ed3434cd530fd83
Emilie <emilie.chnvt@gmail.com>
2024-11-10 15:20:38 +0100

    function fixedNav added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const button = document.getElementById("fixed-btn");
-
 const bottomOfBtn = button.offsetTop;
 
 
@@ -11,3 +10,20 @@ function fixedBtn(){
     }
 }
 window.addEventListener("scroll", fixedBtn);
+
+
+const nav = document.getElementById("fixed-nav");
+const containerGrosTitre = document.getElementById("containerGrosTitre");
+const topOfNav = nav.offsetTop;
+
+function fixedNav(){
+    if(window.scrollY >= topOfNav){
+        containerGrosTitre.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add("fixed-nav");
+    }else{
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove("fixed-nav");
+    }
+}
+window.addEventListener("scroll", fixedNav);
+
