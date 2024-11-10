43cc76dd1b13b72f88a6fae1ce80660da1ff7a98
Emilie <emilie.chnvt@gmail.com>
2024-11-10 15:00:17 +0100

    fixedBtn function added but problems whith scrollY

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//alert("Hello World!");
+const button = document.getElementById("fixed-btn");
+
+const bottomOfBtn = button.offsetTop;
+
+
+function fixedBtn(){
+    if(window.scrollY >= bottomOfBtn){
+        document.body.classList.add("fixed-btn");
+    }else{
+        document.body.classList.remove("fixed-btn");
+    }
+}
+window.addEventListener("scroll", fixedBtn);
