7548e62fa5c9d21611626c7f890abd377529d463
Emilie <emilie.chnvt@gmail.com>
2024-11-10 15:02:00 +0100

    bottomOfBtn / 2

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const bottomOfBtn = button.offsetTop;
 
 
 function fixedBtn(){
-    if(window.scrollY >= bottomOfBtn){
+    if(window.scrollY >= (bottomOfBtn / 2)){
         document.body.classList.add("fixed-btn");
     }else{
         document.body.classList.remove("fixed-btn");
